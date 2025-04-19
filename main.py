b2c6090d07366a1d52c47c29b445e3512c84ce46
nhungbi <nhungvan16@gmail.com>
2025-04-19 18:38:47 -0400

    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+from face_tracker.tracking import Tracker
+from YOLO_test.YOLO import yolo_object_detection_v11, object_description_generator
+
+def main():
+
+    # Initialize the face tracker
+    tracker = Tracker()
+
+    # Parse the scenario
+    
+
